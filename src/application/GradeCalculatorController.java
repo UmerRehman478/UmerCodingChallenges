a064ec69daf6b119b7a0939969065b1d826e20e4
CS219-user <CS219-user@10.0.0.207>
2023-02-13 00:14:34 +0000

    Revert "Revert "Add error label for project grade.""
    This reverts commit db5669784efeb1f0af7439f82f651dc4e3b6de38.

--- src/application/GradeCalculatorController.java
+++ src/application/GradeCalculatorController.java
@@ -20,6 +20,9 @@ public class GradeCalculatorController {
     
     @FXML
     private Label courseGradeLabel; 
+    
+    @FXML
+    private Label projectErrorLabel;
 
     @FXML
     void calculateGrade(ActionEvent event) {
