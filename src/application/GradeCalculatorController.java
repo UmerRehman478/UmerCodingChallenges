65dd396e6911da98630ecf73ec57c4b09d086762
CS219-user <CS219-user@10.0.0.207>
2023-02-14 06:25:56 +0000

    Revert "Added a label error message for when the user inputs a invalid project grade."
    This reverts commit 9c96ad5d086df2ecc04241a2f05032cbfc88e5f3.

--- src/application/GradeCalculatorController.java
+++ src/application/GradeCalculatorController.java
@@ -22,9 +22,6 @@ public class GradeCalculatorController {
     private Label courseGradeLabel; 
 
     @FXML
-    private Label projectErrorLabel; 
-    
-    @FXML
     void calculateGrade(ActionEvent event) {
     	double courseGrade = 0.0;
     	
