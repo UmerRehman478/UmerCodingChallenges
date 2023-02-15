047d334292e0976d9a67a26bbb1702107f0c0c94
CS219-user <CS219-user@10.0.0.207>
2023-02-15 22:59:32 +0000

    Revert "Revert "Reverted everything and added new functions and changes""
    This reverts commit 7603b2d425df082e57135d70f3d898ae99c8e5ab.

--- src/application/GradeCalculatorController.java
+++ src/application/GradeCalculatorController.java
@@ -20,13 +20,69 @@ public class GradeCalculatorController {
     
     @FXML
     private Label courseGradeLabel; 
+    
+    @FXML
+    private Label projectGradeErrorLabel;
+    
+    /**
+     * Checks if the value provided is a valid project grade. A project grade must be numeric and 
+     * a percentage (between 0 and 100). If valid, the equivalent double is returned, if not, this method returns zero.
+     *   
+     * @param valueEntered the value entered as the project grade 
+     * @return the double value of valueEntered if it is numeric and a valid percentage and 0 otherwise.
+     */
+    double getProjectGrade(String valueEntered) {
+    	// Check that the user entered a numeric value
+    	boolean validProjectGrade = true;
+    	boolean singalDecimalPoint = false;
+    	//For loop through each character in string 
+    	for (char c : valueEntered.toCharArray()) {
+    		// If any character is not a digit, set flag to false: it is not a number 
+    		if (!Character.isDigit(c)) {
+    			// Checking if the character is a digit or not
+    			if (c == '.' && !singalDecimalPoint && valueEntered.indexOf(c) == valueEntered.lastIndexOf(c)) {
+    				singalDecimalPoint = true;
+    			} else {
+    			validProjectGrade = false;
+    			projectGradeErrorLabel.setText("Dont include the character: " + c +
+    					". Project grade should be percentage. ");
+    		}
+    		}
+    	}
+    	/* Default project grade to 0. If valid number entered, convert user input to 
+    	 * floating point number.
+    	 */ 
+    	double projectGrade = 0;
+    	if (validProjectGrade) {
+    		projectGrade = Double.parseDouble(valueEntered);
+    	}
+    	
+    	// Check if projectGrade is a valid percentage grade. If not, reset to default grade of 0.
+    	if (projectGrade < 0 || projectGrade > 100) {
+    		projectGradeErrorLabel.setText("Project grade should be between 0% and 100%. Invalid project grade: " 
+    				+ projectGrade);
+    		projectGrade = 0;
+    	} 
+    	
+    	return projectGrade;
+    }
+    
+    /**
+     * Checks and calculates the overall course grade based on the project grade, quiz grade, 
+     * coding challenges passed, and optional coding challenges passed.
+     * @param event when the calculateGrade button is pressed a action occurs, which triggers the calculation of the course grade. 
+     */
 
     @FXML
     void calculateGrade(ActionEvent event) {
+    	projectGradeErrorLabel.setText("");
     	double courseGrade = 0.0;
     	
-    	String projectGrade = projectGradeTextfield.getText();
-    	courseGrade = courseGrade + Double.parseDouble(projectGrade) * 50 / 100;
+    	String projectValueEntered = projectGradeTextfield.getText();
+    	
+    	// Check if user entered a percentage grade. IF not, display error message
+    	double projectGrade  = getProjectGrade(projectValueEntered);
+    	courseGrade = courseGrade + projectGrade * 50 / 100;
     	System.out.println("Project grade entered: " + projectGrade + 
     			" Course grade so far: " + courseGrade);
     	
