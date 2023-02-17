b6f867a1caf06403ef9291191b44c324af3515fa
UmerRehman <CS219-user@10.0.0.207>
2023-02-17 05:47:59 +0000

    Added steps for ccstringsIFandWhile

---
+++ src/basicjava/CCStringsIfAndWhile.java
@@ -0,0 +1,20 @@
+package basicjava;
+
+public class CCStringsIfAndWhile {
+
+	public static boolean isDigit(char c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public static int count(String string, String string2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public static int smallestDigit(int i) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
