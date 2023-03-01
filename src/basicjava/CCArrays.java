1c37001f3595d98a422bfa92bab790a38a4e31ad
UmerRehman <CS219-user@10.0.0.207>
2023-03-01 01:34:29 +0000

    All test pass for CCArrays

--- src/basicjava/CCArrays.java
+++ src/basicjava/CCArrays.java
@@ -1,19 +1,19 @@
 package basicjava;
+
 import java.util.Arrays;
 
 public class CCArrays {
 
 	public static void replace(char[] charArray, char toreplace, char replaceWith) {
-		
-		
-		
-		
+		for (int i = 0; i < charArray.length; i++) {
+	        if (Character.toLowerCase(charArray[i]) == Character.toLowerCase(toreplace)) {
+	            charArray[i] = replaceWith;
+	        }
+	    }
 	}
+		
 	
 	public static void sortAlphabetic(String[] strArray) {
-		
+		Arrays.sort(strArray, String.CASE_INSENSITIVE_ORDER);
 	}
-	
-	
-	
 }
