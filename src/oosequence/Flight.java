9e921d79b69bc1de2629eacbe9334d67bbe80e41
UmerRehman <CS219-user@10.0.0.207>
2023-03-08 02:54:53 +0000

    CC Flight

---
+++ src/oosequence/Flight.java
@@ -0,0 +1,23 @@
+package oosequence;
+
+import java.util.Date;
+
+public class Flight {
+
+	public Object departure;
+	public Object arrival;
+
+	public Flight(Date date, Date date2) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Flight(Flight c) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Object length() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
