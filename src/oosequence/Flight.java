c2c20f85542c1a746d01bbd43b498e5f248f843a
UmerRehman <CS219-user@10.0.0.207>
2023-03-10 02:26:15 +0000

    CC intro classes

--- src/oosequence/Flight.java
+++ src/oosequence/Flight.java
@@ -1,23 +1,42 @@
 package oosequence;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.Date;
+
+import org.junit.Test;
 
 public class Flight {
 
-	public Object departure;
-	public Object arrival;
+	Date departure;
+	Date arrival;
 
-	public Flight(Date date, Date date2) {
-		// TODO Auto-generated constructor stub
+	Flight(Date departuredate, Date arrivaldate2) {
+		if (departuredate == null || arrivaldate2 == null) {
+			departure = departuredate;
+			arrival = arrivaldate2;
+		} else if (departuredate.before(arrivaldate2)) {
+			departure = departuredate;
+			arrival = arrivaldate2;
+		} else {
+			departure = null;
+			arrival = null;
+		}
+	}
+	
+
+	Flight(Flight c) {
+		departure = c.departure;
+	    arrival = c.arrival;
 	}
 
-	public Flight(Flight c) {
-		// TODO Auto-generated constructor stub
-	}
-
-	public Object length() {
-		// TODO Auto-generated method stub
-		return 0;
+	Object length() {
+		long Minutes = 0;
+		if (departure != null && arrival != null) {
+	    	long Millis = arrival.getTime() - departure.getTime();
+	    	Minutes = Millis / (60 * 1000);
+	    }
+        return Minutes;
 	}
 
 }
