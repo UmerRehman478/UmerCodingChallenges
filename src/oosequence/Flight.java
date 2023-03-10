9af8c48f1b8eb5fdbe34a020624e63bb5379d6ce
UmerRehman <CS219-user@10.0.0.207>
2023-03-10 23:35:27 +0000

    CC Encapsulation

--- src/oosequence/Flight.java
+++ src/oosequence/Flight.java
@@ -1,23 +1,66 @@
 package oosequence;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.Date;
+
+import org.junit.Test;
 
 public class Flight {
 
-	public Object departure;
-	public Object arrival;
+	private Date departure;
+	private Date arrival;
 
-	public Flight(Date date, Date date2) {
-		// TODO Auto-generated constructor stub
+	public Flight(Date departuredate, Date arrivaldate2) {
+		if (departuredate == null && arrivaldate2 == null) {
+			departure = departuredate;
+			arrival = arrivaldate2;
+		} else if (departuredate.before(arrivaldate2)) {
+			departure = departuredate;
+			arrival = arrivaldate2;
+		} else {
+			departure = null;
+			arrival = null;
+		}
 	}
+	
 
 	public Flight(Flight c) {
-		// TODO Auto-generated constructor stub
+		departure = c.departure;
+	    arrival = c.arrival;
 	}
 
-	public Object length() {
+	public long length() {
+		long Minutes = 0;
+		if (departure != null && arrival != null) {
+	    	long Millis = arrival.getTime() - departure.getTime();
+	    	Minutes = Millis / (60 * 1000);
+	    }
+        return Minutes;
+	}
+
+
+	public Date getDeparture() {
 		// TODO Auto-generated method stub
-		return 0;
+		return departure;
+	}
+
+
+	public Date getArrival() {
+		// TODO Auto-generated method stub
+		return arrival;
+	}
+
+
+	public void setDeparture(Date date) {
+		// TODO Auto-generated method stub
+		departure = date;
+	}
+
+
+	public void setArrival(Date date) {
+		// TODO Auto-generated method stub
+		arrival = date;
 	}
 
 }
---
+++ src/oosequence/Itinerary.java
@@ -0,0 +1,35 @@
+package oosequence;
+
+import java.util.ArrayList;
+
+public class Itinerary {
+	private ArrayList<Flight> flights;
+	private String name;
+
+	public Itinerary(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return name;
+	}
+
+	public void addFlight(Flight m) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public ArrayList<Flight> getFlights() {
+		// TODO Auto-generated method stub
+		return flights;
+	}
+
+	public long getTotalLayover() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+
+}
