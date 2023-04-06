4cbf296d6caab10444af4972c92b0da23f0f2b00
UmerRehman <CS219-user@10.0.0.207>
2023-04-06 14:56:19 -0600

    File io and Recursion setup only

---
+++ src/basicjava/FileExercises.java
@@ -0,0 +1,15 @@
+package basicjava;
+
+import java.io.IOException;
+
+public class FileExercises {
+
+	public static int counting(String word) {
+		return 0;
+	}
+
+	public void toUpper(String inputFile, String outputFile) throws IOException{
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ src/basicjava/RecursionExercises.java
@@ -0,0 +1,42 @@
+package basicjava;
+
+import java.util.ArrayList;
+
+public class RecursionExercises {
+
+	public Object countDigits(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int addDigits(int i) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public Object sum(int i, int j) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object sumEvenNumbers(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object countVowels(Object object) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object[] removeVowels(Object object) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object sumOfMultiple(ArrayList<Integer> list) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
