736199ab8463b3172918029153412a879d43a082
UmerRehman <CS219-user@10.0.0.207>
2023-04-12 15:56:09 -0600

    CC file io

--- src/basicjava/FileExercises.java
+++ src/basicjava/FileExercises.java
@@ -1,15 +1,42 @@
 package basicjava;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class FileExercises {
 
 	public static int counting(String word) {
-		return 0;
+	    int count = 0;
+	    try (BufferedReader reader = new BufferedReader(new FileReader("example.txt"))) {
+	        String line;
+	        while ((line = reader.readLine()) != null) {
+	            String[] words = line.split(" ");
+	            for (String w : words) {
+	                if (w.equalsIgnoreCase(word)) {
+	                    count++;
+	                }
+	            }
+	        }
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
+	    return count;	
 	}
 
 	public void toUpper(String inputFile, String outputFile) throws IOException{
 		// TODO Auto-generated method stub
+	    try (BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+                BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))) {
+               String line;
+               while ((line = reader.readLine()) != null) {
+                   writer.write(line.toUpperCase());
+                   writer.newLine();
+               }
+           }
+
 	}
 
 }
